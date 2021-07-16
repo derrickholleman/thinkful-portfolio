28b358a7395723b3b11cf2ff6749f1244b6d1577
Derrick Holleman <derrickholleman@gmail.com>
2021-07-16 13:01:26 -0500

    icons

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './Contact.module.css';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 
 const Contact = () => {
     return (
@@ -9,8 +11,8 @@ const Contact = () => {
                 I'd love to hear from you!  Please feel free to contact me at the following:
             </p>
             <ol className={styles.contactLinks}>
-                <li><a href='https://www.linkedin.com/in/derrickholleman/' target='_blank'>LinkedIn</a></li>
-                <li><a href='https://github.com/derrickholleman' target='_blank'>Github</a></li>
+                <li><a href='https://www.linkedin.com/in/derrickholleman/' target='_blank'><AiFillLinkedin /></a></li>
+                <li><a href='https://github.com/derrickholleman' target='_blank'><AiFillGithub /></a></li>
             </ol>
         </article>
     )
