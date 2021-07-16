76f09814608b21119a6269431ea7c3ef04c370b4
Derrick Holleman <derrickholleman@gmail.com>
2021-07-16 00:33:22 -0500

    more setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Nav from './Components/Nav';
+import Intro from './Components/Intro';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-
+        <Nav />
+        <Intro />
+        <About />
+        <Contact />
+        <Footer />
     </div>
   );
 }
---
+++ src/Components/Intro/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Intro.module.css';
+
+const Intro = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Intro
